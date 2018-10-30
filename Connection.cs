94b6740bf05d263ab374c83ed5fd707259337a2b
sagarjadhav143 <43322413+sagarjadhav143@users.noreply.github.com>
2018-10-31 02:12:30 +0530

    Update Connection.cs

--- Connection.cs
+++ Connection.cs
@@ -191,3 +191,205 @@ protected void Page_Load(object sender, EventArgs e)
             }
         }
     }
+
+
+
+----------------------------------------------------------------
+    
+    regController----
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class RegisterController : Controller
+    {
+        MachineEntities db = new MachineEntities();
+        //
+        // GET: /Register/
+        public ActionResult SetDataInDatabase()
+        {
+            return View();
+        }
+         [HttpPost]
+        public ActionResult SetDataInDatabase(LoginPannel model)
+        {
+            LoginPannel tbl = new LoginPannel();
+            tbl.UserName = model.UserName;
+            tbl.Password = model.Password;
+            db.LoginPannels.Add(tbl);
+            db.SaveChanges();
+            return View();
+        }
+         public ActionResult ShowDatabaseForUser()
+         {
+             var item = db.LoginPannels.ToList();
+             return View(item);
+         }
+
+         public ActionResult Delete(int id)
+         {
+             var itm = db.LoginPannels.Where(x => x.Id == id).First();
+             db.LoginPannels.Remove(itm);
+             db.SaveChanges();
+             var itemm= db.LoginPannels.ToList();
+             return View("ShowDatabaseForUser",itemm);
+         }
+
+         public ActionResult Edit(int id)
+         {
+             var itm = db.LoginPannels.Where(x => x.Id == id).First();
+             return View(itm);
+         }
+        [HttpPost]
+         public ActionResult Edit(LoginPannel model)
+         {
+             var itm = db.LoginPannels.Where(x => x.Id == model.Id).First();
+             itm.UserName = model.UserName;
+             itm.Password = model.Password;
+             db.SaveChanges();
+             return View();
+         }
+	}
+}
+
+
+--------save
+@model WebApplication.Models.LoginPannel
+@using (Html.BeginForm())
+{
+    <div class="=container">
+        <div class="form-group">
+            <label>User Name</label>
+            <input class="form-control" name="UserName" placeholder="Enter User Name" />
+        </div>
+        <div class="form-group">
+            <label>Salary</label>
+            <input class="form-control" name="Password" placeholder="Enter Password" />
+        </div>
+        <div class="button">
+            <button>Submit</button>
+        </div>
+    </div>
+}
+
+----------------Show
+
+
+@model List<WebApplication.Models.LoginPannel>
+           <div class="container">
+               <table class="table table-bordered">
+                   <thead>
+                       <tr>
+                           <td>UserName</td>
+                           <td>Password</td>
+                           <td>Action</td>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       @for(int i=0;i<Model.Count;i++)
+                       {
+                           <tr>
+                               <td>@Model[i].UserName</td>
+                               <td>@Model[i].Password</td>
+                               <td>
+                                   <a href="@Url.Action("Delete", new { id =Model[i].Id})"><i class="glyphicon glyphicon-trash"></i></a>
+                                   <a href="#"><i class="glyphicon glyphicon-edit"></i></a>
+                               </td>
+                           </tr>
+                       }
+                   </tbody>
+               </table>
+           </div>
+
+
+--------------------Edit
+
+@model WebApplication.Models.LoginPannel
+  @using (Html.BeginForm())
+{
+    <div class="=container">
+        <div class="form-group">
+            <label>User Name</label>
+            @Html.TextBoxFor(x => x.UserName, new { @class="form-control"})
+        </div>
+        <div class="form-group">
+            <label>User Name</label>
+            @Html.TextBoxFor(x => x.Password, new { @class = "form-control" })
+        </div>
+        <div class="form-group">
+            <label>Salary</label>
+            <input class="form-control" name="Password" placeholder="Enter Password" />
+        </div>
+        <div class="button">
+            <button>Submit</button>
+        </div>
+    </div>
+}
+
+------------------- Model e
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyBusyHotel.Model
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("SuperCategory")]
+        public int supCatId { get; set; }
+        public SuperCategory SuperCategory { get; set; }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public bool isDelete { get; set; }
+
+        public ICollection<SubCategory> SubCategories { get; set; }
+
+
+    }
+}
+
+
+----------------------db context
+using EasyBusyHotel.EntityClasses_Payroll;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBusyHotel.DatabaseContext
+{
+    class DbContexPayroll : DbContext
+    {
+        public DbContexPayroll()
+            : base("EFPayroll")
+        {
+
+        }
+
+        public DbSet<Attendance> Attendance { get; set; }
+        public DbSet<InsentiveDeduction> InsentiveDeduction { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<PublicHoliday> PublicHoliday { get; set; }
+        public DbSet<Shift> Shift { get; set; }
+    }
+}
+
+------------
+remove home
+change rout.config
